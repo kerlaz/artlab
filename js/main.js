8ada496d08febf8cdb4625f031a0cdb486e56194
Inkoda Digital Workshop <inkodaa@gmail.com>
2018-11-20 09:24:55 +0600

    main pages done. TODO: add radial background and video

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ $(document).ready(()=>{
     changeNav();
     window.onscroll = ()=>{changeNav()};
     setInterval(()=>{$('#main-slider .item').toggleClass('on-top')},6000);
+    $('.form-group>input, .form-group>textarea').on('change paste keyup',function () {
+        if($(this).val().length > 0){
+            $(this).addClass('not-empty');
+        } else {
+            $(this).removeClass('not-empty');
+        }
+    })
 });
 function changeNav() {
     if(window.pageYOffset > 10){
